d0b6f13ae4cbc88d5206cdaa62cc8967abe8cce3
Lior Gal <liorgal31892@gmail.com>
2021-08-19 20:03:50 +0300

    added a functionality to the like heart button

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -5,7 +5,16 @@ import React, { Component } from "react";
 
 class Like extends Component {
   render() {
-    return <i className="fa fa-heart-o" aria-hidden="true"></i>;
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+    return (
+      <i
+        onClick={this.props.onClick}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden="true"
+      ></i>
+    );
   }
 }
 

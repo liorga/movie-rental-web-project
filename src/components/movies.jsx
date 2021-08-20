852ad56b3c059bf5ae1de6e5c83f4285aafb937e
liorga <liorgal31892@gmail.com>
2021-08-20 18:40:48 +0300

    added filtering to the listGroup to filter by genre

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -41,11 +41,20 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies,
+    } = this.state;
 
     if (count === 0) return <p>There Are No Movies In The Database.</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered = selectedGenre
+      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+      : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -57,7 +66,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {count} Movies In The Database.</p>
+          <p>Showing {filtered.length} Movies In The Database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -95,7 +104,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}

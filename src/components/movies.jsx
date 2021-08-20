96b4e0d538e530b2665a01b630caad4c5036faf6
Lior Gal <liorgal31892@gmail.com>
2021-08-20 12:21:29 +0300

    added Pagination Componnent to the project with active functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/MovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -20,8 +23,13 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
 
     if (count === 0) return <p>There Are No Movies In The Database.</p>;
 
@@ -64,6 +72,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }

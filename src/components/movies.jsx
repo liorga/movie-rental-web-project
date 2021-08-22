def439451108b248d5a28fe3be8fa6ceceeaa1bf
liorga <liorgal31892@gmail.com>
2021-08-22 12:17:00 +0300

    added updated sorting to movieTable promote to a class

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -42,14 +42,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -87,6 +80,7 @@ class Movies extends Component {
           <p>Showing {filtered.length} Movies In The Database.</p>
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}

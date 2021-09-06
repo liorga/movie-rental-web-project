56dce932bf73e822a452de59a68fa74c3035fbbb
Lior Gal <liorgal31892@gmail.com>
2021-09-06 13:03:40 +0300

    populating the movies and genres to new form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
-import { getMovie, saveMovie } from '../services/MovieService';
-import { getGenres } from '../services/GenreService';
+import { getMovie, saveMovie } from '../services/movieService';
+import { getGenres } from '../services/genreService';
 
 class MovieForm extends Form {
   state = {
@@ -28,17 +28,27 @@ class MovieForm extends Form {
     dailyRentalRate: Joi.number().min(0).max(10).label('Daily Rental Rate'),
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async populateGenres() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === 'new') return;
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === 'new') return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace('/not-found');
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace('/not-found');
+    }
+  }
 
-    this.setState({ data: this.mapToViewModel(movie) });
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,16 @@
 import http from './httpService';
+import { apiUrl } from '../config.json';
 
-const apiEndpoint = 'http://localhost:3900/api/movies';
+const apiEndpoint = apiUrl + '/movies';
 export function getMovies() {
+  return http.get(apiEndpoint);
+}
+
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + '/' + movieId);
+}
+
+export function saveMovie(movie) {
   return http.get(apiEndpoint);
 }
 

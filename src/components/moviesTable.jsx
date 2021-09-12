99b31f1e4f0559421cb427d03ff10c820932587a
Lior Gal <liorgal31892@gmail.com>
2021-09-12 17:27:02 +0300

    added more admin protaction

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,38 +1,48 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Like from "./common/like";
-import Table from "./common/table";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import auth from '../services/authService';
+import Like from './common/like';
+import Table from './common/table';
 
 class MoviesTable extends Component {
   columns = [
     {
-      path: "title",
-      label: "Title",
+      path: 'title',
+      label: 'Title',
       content: (movie) => (
         <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
       ),
     },
-    { path: "genre.name", label: "Genre" },
-    { path: "numberInStock", label: "Stock" },
-    { path: "dailyRentalRate", label: "Rate" },
+    { path: 'genre.name', label: 'Genre' },
+    { path: 'numberInStock', label: 'Stock' },
+    { path: 'dailyRentalRate', label: 'Rate' },
     {
-      key: "like",
+      key: 'like',
       content: (movie) => (
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       ),
     },
-    {
-      key: "delete",
-      content: (movie) => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: 'delete',
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className='btn btn-danger btn-sm'
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;

c163ce765824d3660515f7f3fe1273ec819f15b2
Lior Gal <liorgal31892@gmail.com>
2021-09-12 13:58:37 +0300

    login a user

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,20 +1,32 @@
-import React from "react";
-import Form from "./common/form";
-import Joi from "joi-browser";
+import React from 'react';
+import Form from './common/form';
+import Joi from 'joi-browser';
+import { login } from '../services/authService';
 
 class LoginForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { username: '', password: '' },
     errors: {},
   };
 
   schema = {
-    username: Joi.string().required().label("Username"),
-    password: Joi.string().required().label("Password"),
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password'),
   };
 
-  doSubmit = () => {
-    console.log("submitted");
+  doSubmit = async () => {
+    const { data } = this.state;
+    await login(data.username, data.password);
+    // try {
+    //   await login(data.username, data.password);
+    // } catch (ex) {
+    //   console.log(ex.response.status);
+    //   if (ex.response && ex.response.status === 400) {
+    //     const errors = { ...this.state.errors };
+    //     errors.username = ex.response.data;
+    //     this.setState({ errors });
+    //   }
+    // }
   };
 
   render() {
@@ -22,9 +34,9 @@ class LoginForm extends Form {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderButton("Login")}
+          {this.renderInput('username', 'Username')}
+          {this.renderInput('password', 'Password', 'password')}
+          {this.renderButton('Login')}
         </form>
       </div>
     );
